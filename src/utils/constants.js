86458c2f05d969639183b4b5fbf99ed3d4035126
MukulDhy <1029mukul38@gmail.com>
2025-06-01 15:59:26 +0530

    last constant file changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,6 @@
 // export const WS_URL = "ws://192.168.1.7:5000"; // Change to wss:// for production
-<<<<<<< HEAD
-export const WS_URL = "wss://simiriti-backend.onrender.com/"; // Use secure WebSocket in production
-=======
-// https://simiriti-backend.onrender.com/
 
-export const WS_URL = "wss://simiriti-backend.onrender.com";
->>>>>>> ecf98a406b519dc8a07bafd8668556ce59233b50
+export const WS_URL = "wss://simiriti-backend.onrender.com/"; // Use secure WebSocket in production
+
+
+// export const WS_URL = "wss://simiriti-backend.onrender.com";
